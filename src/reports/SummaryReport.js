ab97c127f472145cca931963305e51641939d357
Matthew Yeh <matthewyeh@Matthews-MacBook-Pro.local>
2020-08-03 21:36:11 -0500

    summary report calculations

--- src/reports/SummaryReport.js
+++ src/reports/SummaryReport.js
@@ -77,6 +77,49 @@ const SummaryReport = (props) => {
         ...transfers];
     }
 
+
+    async function getNetExpenses(groups) {
+      const expenses = {}
+      Object.keys(groups).map(month => {
+        expenses[month] = groups[month].reduce(function (a,b) {
+          if (b.type === 'EXPENSE') {
+            return a + b.amount;
+          }
+          else if (b.type === 'CREDIT') {
+            return a - b.amount;
+          }
+          return a;
+        }, 0)
+      })
+      return expenses;
+    }
+
+    async function getTotalInflow(groups) {
+      const inflows = {}
+      Object.keys(groups).map(month => {
+        inflows[month] = groups[month].reduce(function (a,b) {
+          if (b.type === 'INFLOW') {
+            return a + b.amount;
+          }
+          return a;
+        }, 0)
+      })
+      return inflows;
+    }
+
+    async function getTotalOutflow(groups) {
+      const inflows = {}
+      Object.keys(groups).map(month => {
+        inflows[month] = groups[month].reduce(function (a,b) {
+          if (b.type === 'OUTFLOW') {
+            return a + b.amount;
+          }
+          return a;
+        }, 0)
+      })
+      return inflows;
+    }
+
     async function manipulateData() {
       const today = new Date();
       const currEndMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
@@ -84,10 +127,50 @@ const SummaryReport = (props) => {
       const investments = await getInvestments();
 
       const allDates = [];
+      const netExpensesByDate = {investment: 'Net Expenses'}
+      const inflowsByDate = {investment: 'Total Inflows'}
+      const outflowsByDate = {investment: 'Total Outflows'}
+
       const investmentData = await Promise.all(
         investments.map(async (investment) => {
           const data = await fetchData(investment.id);
           const groups = groupByMonth(data);
+
+          const netExpenses = await getNetExpenses(groups)
+          const inflows = await getTotalInflow(groups)
+          const outflows = await getTotalOutflow(groups)
+
+          console.log(netExpenses)
+          Object.keys(netExpenses).map(date => {
+            const dateFormat = moment(new Date(date)).format('L')
+            console.log(netExpenses[date])
+            if (date in netExpensesByDate) {
+              netExpensesByDate[dateFormat] += netExpenses[date] === undefined ? 0 : netExpenses[date]
+            }
+            else {
+              netExpensesByDate[dateFormat] = netExpenses[date] === undefined ? 0 : netExpenses[date]
+            }
+          })
+
+          Object.keys(inflows).map(date => {
+            date = moment(new Date(date)).format('L')
+            if (date in inflowsByDate) {
+              inflowsByDate[date] += inflows[date] === undefined ? 0 : inflows[date]
+            }
+            else {
+              inflowsByDate[date] = inflows[date] === undefined ? 0 : inflows[date]
+            }
+          })
+
+          Object.keys(outflows).map(date => {
+            date = moment(new Date(date)).format('L')
+            if (date in outflowsByDate) {
+              outflowsByDate[date] += outflows[date] === undefined ? 0 : outflows[date]
+            }
+            else {
+              outflowsByDate[date] = outflows[date] === undefined ? 0 : outflows[date]
+            }
+          })
 
           var minDate = new Date(Math.min(...Object.keys(groups).map(date => new Date(date))));
 
@@ -109,7 +192,37 @@ const SummaryReport = (props) => {
         })
       )
 
+      // sumNAVs
+      const sumNAVs = {investment: 'Sum NAV'}
+      allDates.map(date => {
+        if (!(date in netExpensesByDate)) {
+          netExpensesByDate[date] = 0;
+        }
+        if (!(date in inflowsByDate)) {
+          inflowsByDate[date] = 0;
+        }
+        if (!(date in outflowsByDate)) {
+          outflowsByDate[date] = 0;
+        }
+
+        investmentData.map(investment => {
+          if (date in sumNAVs) {
+            sumNAVs[date] += investment[date] === undefined ? 0 : investment[date]
+          }
+          else {
+            sumNAVs[date] = investment[date] === undefined ? 0 : investment[date]
+          }
+        })
+      })
+
       investmentData.push({investment: ' '})
+      investmentData.push(sumNAVs)
+
+      console.log(netExpensesByDate)
+      investmentData.push(netExpensesByDate)
+      investmentData.push(inflowsByDate)
+      investmentData.push(outflowsByDate)
+
 
       // sort dates
       allDates.sort(function(a,b) {
@@ -123,6 +236,20 @@ const SummaryReport = (props) => {
         }
         return 0;
       })
+
+      const gainsByDate = {investment: 'Gains'}
+      for (let i = 0; i < allDates.length; i++) {
+        const currDate = allDates[i];
+        if (i===0) {
+          gainsByDate[currDate] = sumNAVs[currDate] - inflowsByDate[currDate] + outflowsByDate[currDate] + netExpensesByDate[currDate];
+        }
+        else {
+          const prevDate = allDates[i-1];
+          gainsByDate[currDate] = sumNAVs[currDate] - sumNAVs[prevDate] - inflowsByDate[currDate] + outflowsByDate[currDate] + netExpensesByDate[currDate];
+        }
+      }
+
+      investmentData.push(gainsByDate)
       setMoneyColumns(allDates);
       // console.log(allDates)
       setColumns(['Investment', ...allDates]);
@@ -141,8 +268,7 @@ const SummaryReport = (props) => {
   }
   return (<MaintenanceTable name={"Summary Report"} data={data}
             columns={columns} frozenColumns={frozenColumns}
-            moneyColumns={moneyColumns}
-            sumColumns={true}/>);
+            moneyColumns={moneyColumns}/>);
 }
 
 export default SummaryReport;
