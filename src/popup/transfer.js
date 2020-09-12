35dd4bca68b84d40341aa6b28003ab8b4b034b32
Matthew Yeh <matthewyeh@Matthews-MacBook-Pro.local>
2020-09-12 13:32:49 -0500

    fix transfer new and edit

--- src/popup/transfer.js
+++ src/popup/transfer.js
@@ -2,13 +2,13 @@ import React, { lazy } from 'react';
 import ReactDOM from 'react-dom';
 
 import {getInvestments} from '../serverAPI/investments.js'
-const NewPopup = lazy(() => import('./popup'));
-const FilledFormSheet = lazy(() => import("./popupFilled.js"))
+const NewPopup = lazy(() => import('./popupNew'));
+const EditPopup = lazy(() => import("./popupEdit.js"))
 
 const Transfer = (props) => {
   if (props.match.params.dataID !== undefined) {
     return (
-      <FilledFormSheet
+      <EditPopup
       dataID={props.match.params.dataID}
       dataType="TRANSFER"/>
     )
